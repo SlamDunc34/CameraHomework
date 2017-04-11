d38a6b832984151839e52d8272d9c07715569812
Duncan Henderson <duncanroberthenderson@gmail.com>
2017-04-11 21:29:02 +0100

    added test to remove analog camera. Created tests to add and reove digital cameras

--- CameraTest.java
+++ CameraTest.java
@@ -2,13 +2,39 @@ import static org.junit.Assert.assertEquals;
 import org.junit.*;
 
 public class CameraTest{
-  Photographer = photographer;
-  AnalogCamera = analogCamera;
-  DigitalCamera = digitalCamera;
+  Photographer photographer;
+  AnalogCamera analogCamera;
+  DigitalCamera digitalCamera;
 
   @Before
   public void before(){
-    photographer = new Photographer();
+    photographer = new Photographer("Dunc");
     analogCamera = new AnalogCamera();
     digitalCamera = new DigitalCamera();
   }
+
+  @Test
+  public void canAddCamera(){
+    this.photographer.addCamera(this.analogCamera);
+    assertEquals(1, this.photographer.countCamera());
+  }
+
+  @Test
+  public void canAddCamera(){
+    this.photographer.addCamera(this.digitalCamera);
+    assertEquals(1, this.photographer.countCamera());
+  }
+
+  @Test
+  public void canRemoveCamera(){
+    this.photographer.removeCamera(this.analogCamera);
+    assertEquals(0, this.photographer.countCamera());
+  }
+
+  @Test
+  public void canRemoveCamera(){
+    this.photographer.removeCamera(this.digitalCamera);
+    assertEquals(0, this.photographer.countCamera());
+  }
+}
+
--- Photographer.java
+++ Photographer.java
@@ -3,4 +3,26 @@ import java.util.*;
 public class Photographer{
   
   private String name;
-  private ArrayList<Printable> ;
+  private ArrayList<Printable> prints;
+
+  public Photographer(String name){
+    this.prints = new ArrayList<Printable>();
+    this.name = name;
+  }
+
+  public void addCamera(Printable printable){
+    prints.add(printable);
+  }
+
+
+  public void removeCamera(){
+    prints.clear();
+  }
+
+  public int countCamera(){
+    return prints.size();
+
+  }
+}
+
+
